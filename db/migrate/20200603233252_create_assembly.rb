2bcabf546dedd2999a523678f34c08362f89e8fe
JuanesLinux <juanesph@gmail.com>
2020-06-03 19:42:33 -0400

    Create wine assembly strain

---
+++ db/migrate/20200603233252_create_assembly.rb
@@ -0,0 +1,9 @@
+class CreateAssembly < ActiveRecord::Migration[5.2]
+  def change
+    create_table :assemblies do |t|
+      t.float :percentage
+      t.references :wine, foreign_key: true
+      t.references :strain, foreign_key: true
+    end
+  end
+end
